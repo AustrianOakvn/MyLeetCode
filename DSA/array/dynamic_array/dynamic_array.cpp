fd7ebeefa56c70df38a199202792f2e2ddafcd45
AustrianOakvn <austrian.oak.vn@gmail.com>
2024-07-15 23:03:25 +0900

    dynamic array operations frame

---
+++ DSA/array/dynamic_array/dynamic_array.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Array{
+  int*A;
+  int size;
+  int length;
+};
+
+int Display(struct Array arr){
+
+}
+
+void Insert(struct Array *arr, int index, int x){
+
+}
+
+int Delete(struct Array *arr, int index){
+
+}
+
+int LinearSearch(struct Array *arr, int key){
+
+}
+
+int Sum(struct Array *arr){
+
+}
